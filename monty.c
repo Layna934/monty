8e323ee5ead9cc2f1c4e3562090ed751b0781bcf
Joseph Omoruwou <josephomoruwou@gmail.com>
2024-02-02 03:26:59 +0100

    solved for main program and header file

---
+++ monty.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+ * main - 
+ *
+ */
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;	/* Initialize the stack pointer to NULL */
+
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	size_t read;
+	char *opcode;
+	char *rest = line;
+	char *arg;
+
+	/* Array of instructions */
+	instruction_t instructions[] = {
+		{"push", handle_push},
+		{"push", handle_pall},
+		{NULL, NULL}
+	};
+
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "USAGE: %s file\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != 1)
+	{
+		/**
+		 * Tokenize the line to seperate opcode and argument if any 
+		 * Parse and execure the instrustions using the instruction_t structure
+		 * Update the stack based on the executed instructions
+		 */
+		opcode = strtok_r(line, " \n", &rest);
+		arg = strtok_r(line, " \n", &rest);
+		
+		execute_instructions(&stack, opcode, arg);
+		}
+	}
+	free(line);
+	fclose(file);
+	return (0);
+}
